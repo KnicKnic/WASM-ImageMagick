6a5558e4ce65ac3eeebbbae8d7a830a2bbccaa90
KnicKnic <35521284+KnicKnic@users.noreply.github.com>
2018-07-28 22:55:46 -0700

    add service worker so that sample is accessible offline

--- samples/cmdline/sw.js
+++ samples/cmdline/sw.js
@@ -1,6 +1,7 @@
 // version 1
 // v0
-var CACHE_NAME = 'cache_1_72';
+var prefix_name = "imagemagick_"
+var CACHE_NAME = prefix_name + 'cache_1_0_4';
 var cacheWhitelist = [CACHE_NAME];
 var urlsToCache = [
     './',
@@ -8,19 +9,16 @@ var urlsToCache = [
     './magick.js',
     './magick.wasm',
     './magickApi.js',
-    './sw.js',
     'https://cdn.jsdelivr.net/g/filesaver.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js'
 ];
 // var urlsToCache=[];
-// need to get the current scope and add it to the cache_name
-// then only invalidate cache's that don't start with my scope token
 self.addEventListener('activate', function(event) {  
     event.waitUntil(
       caches.keys().then(function(cacheNames) {
         return Promise.all(
           cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
+            if (cacheName.startsWith(prefix_name) && cacheWhitelist.indexOf(cacheName) ===-1) {
                 //alert("asdf")
                 //self.postMessage("deleting cache " + cacheName)
               return caches.delete(cacheName);
