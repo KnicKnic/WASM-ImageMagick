fd596eef05762b061f63763f4cc14c5809648af2
Sebastian Gurin <sebastigurin@gmail.com>
2018-11-11 06:45:10 -0300

    ci latest node

--- spec/formatSpec.ts
+++ spec/formatSpec.ts
@@ -6,20 +6,23 @@
 
 //   fit('should be equal', async done=>{
 //     // expect(false).toBe(true)
-//     const promises = []
+//     const promises:Promise<boolean>[] = []
 //     formats.forEach(f1=>{
 //       formats.filter(f2=>f2!=f1).forEach(async f2=>{
 //         promises.push(compare(await buildInputFile(`to_rotate.${f1}`), await buildInputFile(`to_rotate.${f2}`)))
 //       })
 //     })
-// console.log(promises);
+// console.log({promises});
 
 //     const results = await pmap(promises, p=>p, {concurrency: 1})
 //     // const results = await    Promise.all(promises)
     
+// console.log({results});
 //     results.forEach(r=>{
 //       expect(r).toBe(false)
 //     })
+//     console.log('done');
+    
 //     done()
 //   })
 // })
