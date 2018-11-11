df1941e36733562812ed2fc31a58f0a5514265fc
Sebastian Gurin <sebastigurin@gmail.com>
2018-11-11 03:47:55 -0300

    gulpfile moved

--- spec/gulpfile.js
+++ spec/gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
-
 gulp.task('jasmine', function () {
-  return gulp.src(['dist/bundle.js', 'dist/assets/**/*'])
+  return gulp.src(['../dist/bundle.js', '../dist/assets/**/*'])
     .pipe(jasmineBrowser.specRunner({ console: true }))
     .pipe(jasmineBrowser.headless({ driver: 'chrome' })); 
 });
 gulp.task('jasmine-server', function () {
-  return gulp.src(['dist/bundle.js', 'dist/assets/**/*'])
+  return gulp.src(['../dist/bundle.js', '../dist/assets/**/*'])
     .pipe(jasmineBrowser.specRunner({ console: true }))
     .pipe(jasmineBrowser.server());
 });
