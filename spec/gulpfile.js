c9f5e3c8c9906c8026760efa9c1c3350872bdd6a
Sebastian Gurin <sebastigurin@gmail.com>
2018-11-11 04:36:03 -0300

    gulp moved - puppeteer downgraded

--- spec/gulpfile.js
+++ spec/gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
+const src = ['../dist/bundle.js', '../dist/assets/**/*']
 
 gulp.task('jasmine', function () {
-  return gulp.src(['dist/bundle.js', 'dist/assets/**/*'])
+  return gulp.src(src)
     .pipe(jasmineBrowser.specRunner({ console: true }))
     .pipe(jasmineBrowser.headless({ driver: 'chrome' })); 
 });
 gulp.task('jasmine-server', function () {
-  return gulp.src(['dist/bundle.js', 'dist/assets/**/*'])
+  return gulp.src(src)
     .pipe(jasmineBrowser.specRunner({ console: true }))
     .pipe(jasmineBrowser.server());
 });
