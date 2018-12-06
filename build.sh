ab05cab6d192479984cfec82022e8e6c86d1df7c
Sebastian Gurin <sebastigurin@gmail.com>
2018-12-06 09:02:35 -0300

    -s EXIT_RUNTIME=1

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@ export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/lib
 export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
 export CFLAGS="-O3"
 export CXXFLAGS="$CFLAGS"
-MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1"
-
-# USE_FREETYPE=1 -s USE_HARFBUZZ=1 
+MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1" # USE_FREETYPE=1 -s USE_HARFBUZZ=1 
 
 # export CFLAGS="-O0 -g2"
 # export CXXFLAGS="$CFLAGS"
@@ -14,46 +12,46 @@ MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME
 export PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
 export PNG_LIBS="-L/code/libpng -L/code/libpng/.libs"
 
-# cd /code/zlib
-# emconfigure ./configure --static
-# emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+cd /code/zlib
+emconfigure ./configure --static
+emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
-# cd /code/libjpeg
-# autoreconf -fvi
-# emconfigure ./configure --disable-shared
-# emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+cd /code/libjpeg
+autoreconf -fvi
+emconfigure ./configure --disable-shared
+emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
-# cd /code/libpng
-# libtoolize
-# # aclocal
-# autoreconf
-# automake --add-missing
-# # ./autogen
-# emconfigure ./configure --disable-shared
-# emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
-
-# cd /code/libtiff
-# libtoolize --force
-# ###
+cd /code/libpng
+libtoolize
 # aclocal
-# ###
-
-# autoreconf --force
-# #### 
-# automake --add-missing
+autoreconf
+automake --add-missing
 # ./autogen
-# autoconf
-# autoreconf
-# ####
+emconfigure ./configure --disable-shared
+emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
-# emconfigure ./configure --disable-shared
-# emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+cd /code/libtiff
+libtoolize --force
+###
+aclocal
+###
+
+autoreconf --force
+#### 
+automake --add-missing
+./autogen
+autoconf
+autoreconf
+####
+
+emconfigure ./configure --disable-shared
+emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
 
 cd /code/ImageMagick
 autoconf
 #emconfigure ./configure --prefix=/ --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x --disable-largefile --disable-openmp --without-bzlib --without-dps --without-freetype --without-jbig --without-openjp2 --without-lcms --without-wmf --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp --without-xml PKG_CONFIG_PATH="/code/libpng:/code/libpng/.libs:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
-emconfigure ./configure --prefix=/ --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x --disable-largefile --disable-openmp --without-bzlib --without-dps --with-freetype --without-jbig --without-openjp2 --without-lcms --without-wmf --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp --without-xml PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
+emconfigure ./configure --prefix=/ --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x --disable-largefile --disable-openmp --without-bzlib --without-dps --without-freetype --without-jbig --without-openjp2 --without-lcms --without-wmf --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp --without-xml PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
 emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
 #produce the correct output file
