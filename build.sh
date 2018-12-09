3383c4ed05b6ebe0faa92f0817850ab496f71f43
Sebastian Gurin <sebastigurin@gmail.com>
2018-12-09 06:01:02 -0300

    build fonts

--- build.sh
+++ build.sh
@@ -1,23 +1,34 @@
-export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/libtiff"
-export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
+export CPPFLAGS="-I/code/prefix/include"
+export LDFLAGS="-L/code/prefix/lib"
 export MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1"
 export CFLAGS="-O3 $CPPFLAGS $MAKE_FLAGS"
 export CXXFLAGS="$CFLAGS"
-export EM_BUILD_VERBOSE=1
-export PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
-export PNG_LIBS="-L/code/libpng -L/code/libpng/.libs"
+# export EM_BUILD_VERBOSE=1
+export PKG_CONFIG_PATH="/code/lib/pkgconfig"
+export PNG_LIBS="$LDFLAGS"
 
 cd /code/zlib
-emconfigure ./configure --prefix=. --static  
+emconfigure ./configure --prefix=/code/prefix --static  
+echo "
+ zlib 'configure' exit code: $?
+"
 emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+echo "
+ zlib 'configure' exit code: $?
+"
 emcmake make install
 
 cd /code/libjpeg
 autoreconf -fvi
-emconfigure ./configure --prefix=. --disable-shared
+emconfigure ./configure --prefix=/code/prefix --disable-shared
+echo "
+ libjpeg 'configure' exit code: $?
+"
 emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+echo "
+ libjpeg 'make' exit code: $?
+"
 emcmake make install
-
 
 cd /code/libpng
 libtoolize
@@ -25,8 +36,14 @@ libtoolize
 autoreconf
 automake --add-missing
 # ./autogen
-emconfigure ./configure --prefix=. --disable-shared
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS"
+emconfigure ./configure --prefix=/code/prefix --disable-shared
+# echo "
+#  libpng 'configure' exit code: $?
+# "
+emcmake make 
+# echo "
+#  libpng 'make' exit code: $?
+# "
 emcmake make install
 
 
@@ -42,15 +59,14 @@ automake --add-missing
 autoconf
 autoreconf
 ####
-emconfigure ./configure --prefix=. --disable-shared
+emconfigure ./configure --prefix=/code/prefix --disable-shared
 emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS"
 emcmake make install
 
 
 cd /code/ImageMagick
 autoconf
-# echo "\n\nPKG_CONFIG_PATH: $PKG_CONFIG_PATH \n\n"
-emconfigure ./configure --prefix=./prefix --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x \
+emconfigure ./configure --prefix=/code/prefix --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x \
   --disable-largefile --disable-openmp --without-bzlib --without-dps --without-jbig --without-openjp2 --without-lcms --without-wmf \
   --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib \
   --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp \
@@ -62,8 +78,8 @@ echo "
 # include emscripten freetype port and its includes files. 
 # heads up - there are some unresolved symbols on link so we need to use emscripten setting -s ERROR_ON_UNDEFINED_SYMBOLS=0 
 export MAKE_FLAGS="$MAKE_FLAGS -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_FREETYPE=1 -s USE_HARFBUZZ=1"
-export CPPFLAGS="$CPPFLAGS -I$HOME/.emscripten_ports/freetype/FreeType-version_1/include"
-export PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
+export CFLAGS="$CFLAGS -I$HOME/.emscripten_ports/freetype/FreeType-version_1/include"
+# export PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
 
 emcmake make CFLAGS="$CFLAGS $CPPFLAGS"
 echo "
@@ -75,9 +91,9 @@ echo "
  ImageMagick 'make install' exit code: $?
 "
 
-# produce the correct output file
-rm -rf utilities/magick.{wasm,js} 
-./libtool --silent --tag=CC --mode=link emcc -verbose $MAKE_FLAGS $CXXFLAGS -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -o utilities/magick.html utilities/magick.o MagickCore/libMagickCore-7.Q16HDRI.la MagickWand/libMagickWand-7.Q16HDRI.la
-echo "
- ImageMagick link exit code: $?
-"
+# # produce the correct output file
+# rm -rf utilities/magick.{wasm,js} 
+# ./libtool --silent --tag=CC --mode=link emcc -verbose $MAKE_FLAGS $CXXFLAGS -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -o utilities/magick.html utilities/magick.o MagickCore/libMagickCore-7.Q16HDRI.la MagickWand/libMagickWand-7.Q16HDRI.la
+# echo "
+#  ImageMagick link exit code: $?
+# "
