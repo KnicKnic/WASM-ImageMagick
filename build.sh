0cb42a0c0da0b6e032d797e357ab51d33aa36221
Sebastián Gurín <sgurin@awalabs.com>
2019-07-09 22:49:19 -0300

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 export CPPFLAGS="-I/code/prefix/include -I$HOME/.emscripten_ports/freetype/FreeType-version_1/include"
 export LDFLAGS="-L/code/prefix/lib"
-#  heads up - there are some unresolved symbols using -s EXIT_RUNTIME=1 that's why we use -s ERROR_ON_UNDEFINED_SYMBOLS=0 
+# HEADS UP! Using "-s ERROR_ON_UNDEFINED_SYMBOLS=0" because "-s USE_FREETYPE=1" gives unresolved symbols on link
 export MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_FREETYPE=1"
 export CFLAGS="-O3 $CPPFLAGS $MAKE_FLAGS"
 export CXXFLAGS="$CFLAGS"
-export EM_BUILD_VERBOSE="0"
 export PKG_CONFIG_PATH="/code/prefix/lib/pkgconfig"
 export PNG_LIBS="$LDFLAGS"
 
@@ -71,19 +70,15 @@ emcmake make install
 
 cd /code/ImageMagick
 autoconf
-
 emconfigure ./configure --prefix=/code/prefix --disable-shared --disable-docs --without-threads --without-magick-plus-plus --without-perl --without-x \
   --disable-largefile --disable-openmp --without-bzlib --without-dps --without-jbig --without-openjp2 --without-lcms --without-wmf \
   --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib \
   --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp \
   PKG_CONFIG_PATH="$PKG_CONFIG_PATH"
 testExitCode "ImageMagick configure" $?
-
 emcmake make CFLAGS="$CFLAGS"
 testExitCode "ImageMagick make" $?
-
 emcmake make install
 testExitCode "ImageMagick make install" $?
-
 ./libtool --silent --tag=CC --mode=link emcc $LDFLAGS $CFLAGS -o utilities/magick.html utilities/magick.o /code/prefix/lib/libMagickCore-7.Q16HDRI.la  /code/prefix/lib/libMagickWand-7.Q16HDRI.la
 testExitCode "ImageMagick link" $?
