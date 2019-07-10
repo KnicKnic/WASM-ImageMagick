e464be02b7930f0d25ea2bfc307fc19e9a178c16
Sebastián Gurín <sgurin@awalabs.com>
2019-07-09 22:02:22 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,59 +1,89 @@
-
-export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/libtiff"
-export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
-export CFLAGS="-O3"
+export CPPFLAGS="-I/code/prefix/include -I$HOME/.emscripten_ports/freetype/FreeType-version_1/include"
+export LDFLAGS="-L/code/prefix/lib"
+#  heads up - there are some unresolved symbols using -s EXIT_RUNTIME=1 that's why we use -s ERROR_ON_UNDEFINED_SYMBOLS=0 
+export MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_FREETYPE=1"
+export CFLAGS="-O3 $CPPFLAGS $MAKE_FLAGS"
 export CXXFLAGS="$CFLAGS"
-MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1"
+export EM_BUILD_VERBOSE="0"
+export PKG_CONFIG_PATH="/code/prefix/lib/pkgconfig"
+export PNG_LIBS="$LDFLAGS"
 
-# export CFLAGS="-O0 -g2"
-# export CXXFLAGS="$CFLAGS"
-# MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s SAFE_HEAP=1 -s ASSERTIONS=1"
+testExitCode  ()
+{
+  name=$1
+  exitCode=$2
+  echo "
 
-export PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
-export PNG_LIBS="-L/code/libpng -L/code/libpng/.libs"
+  $name exit code: $exitCode
+
+  "
+  if [ "$exitCode" -ne "0" ]; then
+    exit 1
+  fi
+}
+
+rm -rf /code/ImageMagick/utilities/magick.{wasm,js} 
+rm -rf /code/prefix
+
 
 cd /code/zlib
-emconfigure ./configure --static
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+emconfigure ./configure --prefix=/code/prefix --static 
+testExitCode "zlib configure" $?
+emcmake make CFLAGS="$CFLAGS"
+testExitCode "zlib make" $?
+emcmake make install
+
 
 cd /code/libjpeg
 autoreconf -fvi
-emconfigure ./configure --disable-shared
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+emconfigure ./configure --prefix=/code/prefix --disable-shared
+testExitCode "libjpeg configure" $?
+emcmake make CFAGS="$CFLAGS $LDFLAGS"
+testExitCode "libjpeg make" $?
+emcmake make install
+
 
 cd /code/libpng
 libtoolize
-# aclocal
 autoreconf
 automake --add-missing
-# ./autogen
-emconfigure ./configure --disable-shared
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+emconfigure ./configure --prefix=/code/prefix --disable-shared
+testExitCode "libpng configure" $?
+emcmake make 
+testExitCode "libpng make" $?
+emcmake make install
+
 
 cd /code/libtiff
 libtoolize --force
-###
 aclocal
-###
-
 autoreconf --force
-#### 
 automake --add-missing
 ./autogen
 autoconf
 autoreconf
-####
-
-emconfigure ./configure --disable-shared
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
+emconfigure ./configure --prefix=/code/prefix --disable-shared
+testExitCode "libtiff configure" $?
+emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS"
+testExitCode "libtiff make" $?
+emcmake make install
 
 
 cd /code/ImageMagick
 autoconf
-#emconfigure ./configure --prefix=/ --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x --disable-largefile --disable-openmp --without-bzlib --without-dps --without-freetype --without-jbig --without-openjp2 --without-lcms --without-wmf --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp --without-xml PKG_CONFIG_PATH="/code/libpng:/code/libpng/.libs:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
-emconfigure ./configure --prefix=/ --disable-shared --without-threads --without-magick-plus-plus --without-perl --without-x --disable-largefile --disable-openmp --without-bzlib --without-dps --without-freetype --without-jbig --without-openjp2 --without-lcms --without-wmf --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp --without-xml PKG_CONFIG_PATH="/code/libpng:/code/zlib:/code/libjpeg:/code/libtiff:/code/libtiff/libtiff:"
-emcmake make $MAKE_FLAGS CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" 
 
-#produce the correct output file
-#/bin/bash ./libtool --silent --tag=CC --mode=link emcc --pre-js /code/webworker.js $MAKE_FLAGS $CXXFLAGS -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -o utilities/magick.html utilities/magick.o MagickCore/libMagickCore-7.Q16HDRI.la MagickWand/libMagickWand-7.Q16HDRI.la 
-/bin/bash ./libtool --silent --tag=CC --mode=link emcc $MAKE_FLAGS $CXXFLAGS -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -o utilities/magick.html utilities/magick.o MagickCore/libMagickCore-7.Q16HDRI.la MagickWand/libMagickWand-7.Q16HDRI.la
+emconfigure ./configure --prefix=/code/prefix --disable-shared --disable-docs --without-threads --without-magick-plus-plus --without-perl --without-x \
+  --disable-largefile --disable-openmp --without-bzlib --without-dps --without-jbig --without-openjp2 --without-lcms --without-wmf \
+  --without-xml --without-fftw --without-flif --without-fpx --without-djvu --without-fontconfig --without-raqm --without-gslib \
+  --without-gvc --without-heic --without-lqr --without-openexr --without-pango --without-raw --without-rsvg --without-webp \
+  PKG_CONFIG_PATH="$PKG_CONFIG_PATH"
+testExitCode "ImageMagick configure" $?
+
+emcmake make CFLAGS="$CFLAGS"
+testExitCode "ImageMagick make" $?
+
+emcmake make install
+testExitCode "ImageMagick make install" $?
+
+./libtool --silent --tag=CC --mode=link emcc $LDFLAGS $CFLAGS -o utilities/magick.html utilities/magick.o /code/prefix/lib/libMagickCore-7.Q16HDRI.la  /code/prefix/lib/libMagickWand-7.Q16HDRI.la
+testExitCode "ImageMagick link" $?
