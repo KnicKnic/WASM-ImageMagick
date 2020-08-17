903f46025b2587c3d6d7d70da2425a83d4f338a3
KnicKnic <knic.knic@gmail.com>
2020-08-16 22:07:23 -0700

    remove clamping as new compiler does it by default, yet still complains...

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/lib
 export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
 export CFLAGS="-O3"
 export CXXFLAGS="$CFLAGS"
-MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s MAXIMUM_MEMORY=4GB"
+MAKE_FLAGS="-s ALLOW_MEMORY_GROWTH=1 -s MAXIMUM_MEMORY=4GB"
 
 # export CFLAGS="-O0 -g2"
 # export CXXFLAGS="$CFLAGS"
