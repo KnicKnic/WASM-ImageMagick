1adf63a9f344783e5ac57daf91d31376c8fe5d7d
KnicKnic <knic.knic@gmail.com>
2020-08-22 22:42:14 -0700

    remove freetype

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/lib
 export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
 export CFLAGS="-O3"
 export CXXFLAGS="$CFLAGS"
-MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1 -s USE_FREETYPE=1"
+MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1"
 
 # export CFLAGS="-O0 -g2"
 # export CXXFLAGS="$CFLAGS"
