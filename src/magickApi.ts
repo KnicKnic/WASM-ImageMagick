ece4b8d21f8949e5b0db8a75e8f0f2f96b9903b7
Nick Maliwacki <knic.knic@gmail.com>
2020-11-22 14:14:37 -0800

    remove stacktrace

--- src/magickApi.ts
+++ src/magickApi.ts
@@ -1,4 +1,4 @@
-import StackTrace from "stacktrace-js"
+// import StackTrace from "stacktrace-js"
 /**
  * Base class for ImageMagick input and output files.
  */
@@ -110,68 +110,68 @@ function GetCurrentUrlDifferentFilename(currentUrl, fileName) {
 }
 let currentJavascriptURL = './magickApi.js';
 
-// // instead of doing the sane code of being able to just use import.meta.url 
-// // (Edge doesn't work) (safari mobile, chrome, opera, firefox all do)
-// // 
-// // I will use stacktrace-js library to get the current file name
-// //
+// instead of doing the sane code of being able to just use import.meta.url 
+// (Edge doesn't work) (safari mobile, chrome, opera, firefox all do)
+// 
+// I will use stacktrace-js library to get the current file name
+//
 // try {
-//   // @ts-ignore
-//   let packageUrl = import.meta.url;
-//   currentJavascriptURL = packageUrl;
+  // @ts-ignore
+  let packageUrl = import.meta.url;
+  currentJavascriptURL = packageUrl;
 // } catch (error) {
 //   // eat
 // }
 
-function GenerateStackAndGetPathAtDepth(depth){
-  try {
-    let stacktrace$$1 = StackTrace.getSync();
-    let filePath = stacktrace$$1[depth].fileName;
+// function GenerateStackAndGetPathAtDepth(depth){
+//   try {
+//     let stacktrace$$1 = StackTrace.getSync();
+//     let filePath = stacktrace$$1[depth].fileName;
     
-    // if the stack trace code doesn't return a path separator
-    if(filePath !== undefined && filePath.indexOf('/') === -1 && filePath.indexOf('\\') === -1){
-      return undefined
-    }
-    return filePath
+//     // if the stack trace code doesn't return a path separator
+//     if(filePath !== undefined && filePath.indexOf('/') === -1 && filePath.indexOf('\\') === -1){
+//       return undefined
+//     }
+//     return filePath
 
-  } catch (error) {
-    return undefined
-  }
-}
+//   } catch (error) {
+//     return undefined
+//   }
+// }
 
-function GetCurrentFileURLHelper3() {
-  // 3rd call site didn't work, so I made this complicated maze of helpers.. 
+// function GetCurrentFileURLHelper3() {
+//   // 3rd call site didn't work, so I made this complicated maze of helpers.. 
 
-  // Pulling the filename from the 3rd call site of the stacktrace to get the full path
-  // to the module. The first index is inconsistent across browsers and does not return 
-  // the full path in Safari and results in the worker failing to resolve. 
+//   // Pulling the filename from the 3rd call site of the stacktrace to get the full path
+//   // to the module. The first index is inconsistent across browsers and does not return 
+//   // the full path in Safari and results in the worker failing to resolve. 
 
-  // I am preferring to do depth 0 first, as that will ensure people that do minification still works
+//   // I am preferring to do depth 0 first, as that will ensure people that do minification still works
 
-  let filePath = GenerateStackAndGetPathAtDepth(0)
-  if(filePath === undefined){
-    filePath = GenerateStackAndGetPathAtDepth(2)
-  }
+//   let filePath = GenerateStackAndGetPathAtDepth(0)
+//   if(filePath === undefined){
+//     filePath = GenerateStackAndGetPathAtDepth(2)
+//   }
 
-  // if the stack trace code messes up 
-  if(filePath === undefined){
-    filePath = './magickApi.js';
-  }
+//   // if the stack trace code messes up 
+//   if(filePath === undefined){
+//     filePath = './magickApi.js';
+//   }
 
-  return filePath
-}
+//   return filePath
+// }
 
-function GetCurrentFileURLHelper2() {
-  return GetCurrentFileURLHelper3();
-}
-function GetCurrentFileURLHelper1() {
-  return GetCurrentFileURLHelper2();
-}
-function GetCurrentFileURL() {
-  return GetCurrentFileURLHelper1();
-}
+// function GetCurrentFileURLHelper2() {
+//   return GetCurrentFileURLHelper3();
+// }
+// function GetCurrentFileURLHelper1() {
+//   return GetCurrentFileURLHelper2();
+// }
+// function GetCurrentFileURL() {
+//   return GetCurrentFileURLHelper1();
+// }
 
-currentJavascriptURL = GetCurrentFileURL();
+// currentJavascriptURL = GetCurrentFileURL();
 
 const magickWorkerUrl = GetCurrentUrlDifferentFilename(currentJavascriptURL, 'magick.js');
 
